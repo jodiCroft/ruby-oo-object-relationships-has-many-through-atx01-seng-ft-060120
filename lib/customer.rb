bbf83e53343bb2873c318a18997fb0b7063c15dd
github username <github email address>
2020-06-07 15:46:21 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,45 @@
+require "pry"
+
 class Customer
+
+    attr_reader :name, :age
+
+    @@all = []
   
+    def initialize(name, age)
+        @name = name
+        @age = age
+
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, total, tip)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select do |meal|
+            meal.customer == self
+        end
+    end
+
+    def waiters
+        customers = Meal.all.select do |meal|
+           meal.customer == self
+        end
+        customers.map do |customer|
+            customer.waiter
+        end
+    end
+
+
+
+
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,48 @@
+require "pry"
 class Waiter
 
+    attr_accessor :name, :years_of_exp
+
+    @@all = []
+
+    
+
+    def initialize(name, years_of_exp)
+        @name = name
+        @years_of_exp = years_of_exp
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        waiter_meals = Meal.all.select do |meal|
+            meal.waiter == self
+        end
+        waiter_meals.select do |meal|
+            meal.waiter
+        end 
+    end
+
+    def best_tipper
+        biggest_tip = 0
+        biggest_tipper = nil
+        self.meals.each do |meal|
+            if meal.tip > biggest_tip
+                biggest_tip = meal.tip
+                biggest_tipper = meal.customer
+            end
+        end
+        biggest_tipper
+    end
+
+
+   
 end
